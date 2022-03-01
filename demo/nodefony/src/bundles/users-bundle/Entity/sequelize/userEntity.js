10a21ab211fb6a716a59fa4a4a7143422950c12a
cci <ccamensuli@gmail.com>
2022-03-01 15:30:02 +0100

    uo/grade deps

--- demo/nodefony/src/bundles/users-bundle/Entity/sequelize/userEntity.js
+++ demo/nodefony/src/bundles/users-bundle/Entity/sequelize/userEntity.js
@@ -82,7 +82,7 @@ class userEntity extends nodefony.Entity {
       },
       email: {
         type: DataTypes.STRING,
-        primaryKey: true,
+        //primaryKey: true,
         unique: true,
         allowNull: false,
         validate: {
